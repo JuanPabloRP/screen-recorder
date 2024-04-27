dde4768ba6ffbc6815d8104799f8b21943ddc6be
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-04-27 17:27:07 -0500

    Add recording functionality to layout.tsx and create recordingContext.tsx

---
+++ context/recordingContext.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { createContext, useContext, useState } from 'react';
+
+/* type RecordingContextType = {
+
+}
+const RecordingContext = createContext<RecordingContextType>(undefined);
+*/
+
+const RecordingContext = createContext<any>(undefined);
+
+export function RecordingWrapper({ children }: { children: React.ReactNode }) {
+	const [recording, setRecording] = useState({
+		video: false,
+		audio: true,
+		screen: true,
+		mic: false,
+		fps: 30,
+		isRecording: false,
+		isPaused: false,
+		videoStream: {},
+	});
+
+	return (
+		<RecordingContext.Provider
+			value={{
+				recording,
+				setRecording,
+			}}
+		>
+			{children}
+		</RecordingContext.Provider>
+	);
+}
+
+export function useRecordingContext() {
+	return useContext(RecordingContext);
+}
