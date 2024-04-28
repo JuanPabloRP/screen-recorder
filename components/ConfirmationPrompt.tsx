5a2fe08d81a69b0fdff5d8cdfcaec793a1fac1b5
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-04-27 19:41:07 -0500

    Add ConfirmationPrompt component for handling beforeunload event in ConfirmationPrompt.tsx

---
+++ components/ConfirmationPrompt.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+const ConfirmationPrompt = () => {
+	useEffect(() => {
+		const handleUnload = (e: any) => {
+			e.preventDefault();
+
+			const confirmationMessage =
+				'¿Estás seguro que quieres salir?, lo que esten grabando se perdera';
+			e.returnValue = confirmationMessage;
+
+			return confirmationMessage;
+		};
+
+		window.addEventListener('beforeunload', (e) => handleUnload(e));
+
+		return () => {
+			window.removeEventListener('beforeunload', (e) => handleUnload(e));
+		};
+	}, []);
+	return (
+		<div>
+			<p></p>
+		</div>
+	);
+};
+
+export default ConfirmationPrompt;
