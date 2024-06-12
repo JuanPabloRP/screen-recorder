dfe0071b225010a7f778e4af7d95008d2f7e9891
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-06-12 17:50:40 -0500

    Cambio en colores en navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,27 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
 const Navbar = () => {
+	const pathname = usePathname();
+
+	const routes = [
+		{
+			name: 'Inicio',
+			path: '/',
+		},
+		{
+			name: 'Empezar a grabar',
+			path: '/recorder',
+		},
+	];
+
+	const getLinkClass = (path: string) => {
+		return pathname === path
+			? 'text-congress-blue-50 underline'
+			: 'text-congress-blue-100/70';
+	};
 	return (
 		<nav
 			className={`flex flex-col  justify-center items-center md:flex-row md:justify-between md:items-center  text-congress-blue-50 md:h-auto bg-[#141414] sticky top-0 z-50`}
@@ -10,13 +30,19 @@ const Navbar = () => {
 				Grabar pantalla
 			</Link>
 
-			<ul className="flex gap-2 mr-5">
-				<li>
-					<Link href={'/recorder'}>Empezar a grabar</Link>
-				</li>
-				{/* <li>
-					<Link href="">Sobre el proyecto</Link>
-				</li> */}
+			<ul className="flex gap-2 mr-5 mb-5 md:mb-0">
+				{routes.map((route, index) => (
+					<li key={index}>
+						<Link
+							href={route.path}
+							className={` font-semibold hover:text-congress-blue-100 ${getLinkClass(
+								route.path
+							)}`}
+						>
+							{route.name}
+						</Link>
+					</li>
+				))}
 			</ul>
 		</nav>
 	);
